ee0aff3748ccc75a4a1e53c734cac1bac4aa28f7
Joshua Phillip <javanphillipp@gmail.com>
2023-10-03 11:32:08 -0400

    Test

--- App/views/user.py
+++ App/views/user.py
@@ -14,6 +14,11 @@ from App.controllers import (
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
+@user_views.route('/r', methods=['GET'])
+def r():
+    return ('users')
+
+
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
     users = get_all_users()
