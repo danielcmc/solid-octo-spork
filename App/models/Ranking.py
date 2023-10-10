7d16f1f8cbdb2fcb5dcf90952fbe987e9a409552
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-10-09 23:52:25 -0400

    Update and rename ranking.py to Ranking.py

--- App/models/Ranking.py
+++ App/models/Ranking.py
@@ -4,7 +4,7 @@ from App.models import Participation
 class Ranking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('Student.id'), nullable=False)
-    competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'), nullable=False)
+    #competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'), nullable=False)
     total_points = db.Column(db.Integer, nullable=False)  # Total points earned by the user
 
     def update_total_points(self):
