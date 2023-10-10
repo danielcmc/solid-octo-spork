7da094fec41d68bd9c47bff47f42cbf644cb3ce8
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-10-09 23:44:11 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -47,12 +47,10 @@ student_cli = AppGroup('student', help='Student object commands')
 @click.argument("password", default="T.Kirk")
 def create_student_command(username, password):
     create_Student(username, password)
-    print(f'{username} is an Student !')
-
 
 ## Use click.prompts
 
-@student_cli.command("participate", help="Student")
+"""@student_cli.command("participate", help="Student")
 @click.argument("id", default="2")
 @click.argument("competition_id", default="1")
 def student_Participate(id, competition_id):
@@ -65,7 +63,38 @@ def student_Participate(id, competition_id):
         else:
             print(f'Competition {competition_id} not found')
     else:
-        print(f'Student {competition_id} not found')
+        print(f'Student {competition_id} not found')"""
+        
+@student_cli.command("register", help="Registers student for competitions")
+@click.argument("username", default="bob")
+@click.argument("competition_name", default="RunTime")
+def register_student_command(username, competition_name):
+    student = Student.query.filter_by(username=username).first()
+    if student:
+      competition = Competition.query.filter_by(name=competition_name).first()
+      if competition:
+        student.participate_in_competition(competition)
+        #competition.add_participant(student)
+      else:
+        print(f'{competition_name} was not found')
+    else:
+      print(f'{username} was not found')
+
+@student_cli.command("view-details", help="displays user information")
+@click.argument("username", default="bob")
+def display_user_info(username):
+  student = Student.query.filter_by(username=username).first()
+  
+  if not student:
+    print(f'{username} is not a valid student username')
+  else:
+    score = get_points(student.id)
+    student.set_points(score)
+    print("Profile Infomation")
+    print(student.get_json())
+    print("Participated in the following competitions:")
+    for comp in student.competitions:
+      print(f'{comp.name} ')
 
 
 """
@@ -109,9 +138,9 @@ def login_user_command(username, password):
 
 # this command will be : flask user create bob bobpass
 
-@student_cli.command("list", help="Lists users in the database")
+@student_cli.command("list", help="Lists students in the database")
 @click.argument("format", default="string")
-def list_user_command(format):
+def list_student_command(format):
     if format == 'string':
         print(get_all_students())
     else:
@@ -119,44 +148,112 @@ def list_user_command(format):
 
 app.cli.add_command(student_cli) # add the group to the cli
 
-admin_cli = AppGroup('Admin', help='User object commands') 
+admin_cli = AppGroup('Admin', help='Admin object commands') 
 
-@admin_cli.command("create", help="Creates a Competition")
+@admin_cli.command("createAdmin", help="Creates an admin")
 @click.argument("username", default="Dave")
 @click.argument("password", default="NBuster")
-def create_admin_command(username, password):
-    create_Admin(username, password)
-    print(f'{username} is an Admin !')
+@click.argument("staff_id", default="1001")
+def create_admin_command(username, password, staff_id):
+    create_Admin(username, password, staff_id)
 
-@admin_cli.command("createCompetition", help="Creates a Competition")
-@click.argument("name", default="RunTime")
-@click.argument("username", default="Dave")
-def create_competition_command(name,username):
+@admin_cli.command("createCompetition", help="Creates a competition")
+@click.argument("competition_name", default="RunTime")
+@click.argument("username", default="rob")
+#@jwt_required()
+def create_competition_command(competition_name, username):
+  #username = get_jwt_identity()
   admin = Admin.query.filter_by(username=username).first()
   if admin:
-    Competition = admin.add_Competition(name)
-    print(f'{name} created by {admin.id}!')
+    create_Competition(competition_name, admin.id)
 
 
-
-@admin_cli.command("list", help="Lists users in the database")
+@admin_cli.command("list", help="Lists admins in the database")
 @click.argument("format", default="string")
 def list_user_command(format):
     if format == 'string':
         print(get_all_admins())
     else:
-        print(get_all_students_json())
+        print(get_all_admins_json())
 
 
-@admin_cli.command("listComp", help="Lists users in the database")
-@click.argument("format", default="string")
-def list_user_command(format):
-    print(get_all_competitions())
-   
+@admin_cli.command("addResults",help="enters scores of participants of the competition")
+@click.argument("admin_username", default="rob")
+@click.argument("student_username", default="bob")
+@click.argument("competition_name", default="RunTime")
+@click.argument("score", default=10)
+def add_results(admin_username, student_username, competition_name, score):
+  comp = Competition.query.filter_by(name=competition_name).first()
+  admin = Admin.query.filter_by(username=admin_username).first()
+  
+  if not admin:
+    print(f'{admin_username} is not an admin')
+    return
+  
+  if not comp:
+    print(f'{competition_name} is not a valid competition')
+    return
+  
+  if comp.creator_id == admin.id:
+    student = Student.query.filter_by(username=student_username).first()
+    
+    if not student:
+      print(f'{student_username} is not a valid username')
+      return
+    
+    for participant in comp.participants:
+      if participant.username == student.username:
+        participation = Participation.query.filter_by(user_id=student.id, competition_id=comp.id).first()
+        participation.update_points(score)
+        participation.points_earned = score
+        db.session.add(participation)
+        db.session.commit()
+        #competition.participants = participation
+        """for participant in comp.participants:
+          if participant.id == participation.id:
+            participant.point_earned = participation.point_earned
+            db.session.add(participant)
+        db.session.commit()"""
+        print("Score added!")
+        return
+        
+    print(f'{student_username} did not participate in {competition_name}')
+    
+  else:
+    print(f'{admin_username} does not have access to add results for {competition_name}')
 
 app.cli.add_command(admin_cli)
 
+user_cli = AppGroup('user', help='User object commands')
 
+@user_cli.command("comp-list", help="Lists competitions in the database")
+def list_comp_command():
+    print(get_all_competitions())
+
+@user_cli.command("participation-list", help="Lists participants in the database")
+def list_participation_command():
+      print(get_all_participations())
+
+@user_cli.command("competition-details", help="displays competition details")
+def display_competition_details():
+  comps = Competition.query.all()
+
+  if not comps:
+    print("No competitions found!")
+  else:
+    for comp in comps:
+      print(comp.get_json())
+      participants = Participation.query.filter_by(competition_id=comp.id).all()
+
+      if not participants:
+        print("No participants found!")
+      else:
+        for participant in participants:
+          #participation = Participation.query.filter_by(user_id=participant.id).first()
+          #if participation:
+          print(participant.get_json())
+
+app.cli.add_command(user_cli)
 
 '''
 Test Commands
