f3c369195307de3f798cbda84cb09f6028977cd2
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-10-09 23:55:27 -0400

    Update Competition.py

--- App/models/Competition.py
+++ App/models/Competition.py
@@ -7,9 +7,15 @@ class Competition(db.Model):
     creator_id = db.Column(db.Integer, db.ForeignKey('admin.id'), nullable=False)
     participants = db.relationship('Student', secondary="participation", overlaps='competetions', lazy=True)
 
-
+    def __init__(self, name, creator_id):
+      self.name = name
+      self.creator_id = creator_id
+    
     def get_json(self):
         return {
             'id': self.id,
             'name': self.name
         }
+
+    def __repr__(self):
+        return f'<Competition {self.id} : {self.name}>'
