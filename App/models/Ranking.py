49d9d6aec88220847878316d6be6eca86d22f475
Joshua Phillip <98168650+SleepyDefender@users.noreply.github.com>
2023-10-10 05:56:52 +0000

    render testing

--- App/models/Ranking.py
+++ App/models/Ranking.py
@@ -3,7 +3,9 @@ from App.models import Participation
 
 class Ranking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Student.id'), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('Student.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
     #competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'), nullable=False)
     total_points = db.Column(db.Integer, nullable=False)  # Total points earned by the user
 
