34319762ca625c8327b4c0730e8319aabd4b9e37
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-10-09 23:57:57 -0400

    Update Participation.py

--- App/models/Participation.py
+++ App/models/Participation.py
@@ -5,16 +5,15 @@ class Participation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
     competition_id = db.Column(db.Integer, db.ForeignKey('competition.id'), nullable=False)
-    rank = db.Column(db.Integer, nullable=False)
-    points_earned = db.Column(db.Integer, nullable=True)
+    #rank = db.Column(db.Integer, nullable=False)
+    points_earned = db.Column(db.Integer, nullable=True, default=0)
 
-
-    def __init__(self, user_id, competition_id, rank):
-        self.user_id = user_id
-        self.competition_id = competition_id
-        self.rank = rank
-        self.points_earned = 0
-
+    def update_points(self, points_earned):
+      self.points_earned = points_earned
+    
+    def __init__(self, user_id, competition_id):
+      self.user_id = user_id
+      self.competition_id = competition_id
 
     # user = db.relationship('Student', back_populates='Competed_In')
     # competition = db.relationship('Competition', back_populates='participants')
@@ -24,6 +23,6 @@ class Participation(db.Model):
             'id': self.id,
             'user_id': self.user_id,
             'competition_id': self.competition_id,
-            'rank': self.rank,
-            'points_earned': self.calculate_points()
+            #'rank': self.rank,
+            'points_earned': self.points_earned
         }
